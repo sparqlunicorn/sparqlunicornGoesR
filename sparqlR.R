fe1ed97f3fde35a8772b2f02e1660e94a29cbba0
florianthiery <web@florian-thiery.de>
2019-09-26 13:50:49 +0200

    Update sparqlR.R

--- sparqlR.R
+++ sparqlR.R
@@ -2,14 +2,15 @@
 library(WikidataQueryServiceR)
 
 # query Wikidata SPARQL endpoint
-# https://w.wiki/8uP
-sparql_query <- 'SELECT ?item ?itemLabel ?motive ?motiveLabel
+# https://w.wiki/8uQ
+sparql_query <- 'SELECT ?item ?itemLabel ?motive ?motiveLabel ?pic
 WHERE
 {
   VALUES ?objects {wd:Q41971267}
-?item wdt:P31 ?objects.
+  ?item wdt:P31 ?objects.
   ?item wdt:P180 ?motive .
-SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" }
+  ?item wdt:P18 ?pic .
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" }
 } ORDER BY ?motiveLabel'
 
 df <- query_wikidata(sparql_query)
@@ -21,5 +22,5 @@ library(ggplot2)
 ggplot()+
   geom_bar(data = df2, aes(x = motiveLabel, fill = motiveLabel))+
   labs(y = "count",
-       title = "Archaeological site")+
+       title = "Greek Amphoras")+
   theme_dark()
